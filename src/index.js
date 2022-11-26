d3ea38be19ffcb7a875ae7fee09760d95e152a20
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2022-11-26 21:44:40 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,48 @@
-function morseCodeDecoder(text) {
+function morseCodeDecoder(code) {
     let alfabeto = {
-        A: '.-',
-        B: '-...',
-        C: '-.-.',
-        D: '-..',
-        E: '.',
-        F: '...',
-        G: '--.',
-        H: '....',
-        I: '..',
-        J: '.---',
-        K: '-.-',
-        L: '.-..',
-        M: '--',
-        N: '-.',
-        O: '---',
-        P: '.--.',
-        Q: '--.-',
-        R: '.-.',
-        S: '...',
-        T: '-',
-        U: '..-',
-        V: '...-',
-        W: '.--',
-        X: '-..-',
-        Y: '-.--',
-        Z: '--..'
+        '.-':     'a',
+        '-...':   'b',
+        '-.-.':   'c',
+        '-..':    'd',
+        '.':      'e',
+        '..-.':   'f',
+        '--.':    'g',
+        '....':   'h',
+        '..':     'i',
+        '.---':   'j',
+        '-.-':    'k',
+        '.-..':   'l',
+        '--':     'm',
+        '-.':     'n',
+        '---':    'o',
+        '.--.':   'p',
+        '--.-':   'q',
+        '.-.':    'r',
+        '...':    's',
+        '-':      't',
+        '..-':    'u',
+        '...-':   'v',
+        '.--':    'w',
+        '-..-':   'x',
+        '-.--':   'y',
+        '--..':   'z',
+        '.----':  '1',
+        '..---':  '2',
+        '...--':  '3',
+        '....-':  '4',
+        '.....':  '5',
+        '-....':  '6',
+        '--...':  '7',
+        '---..':  '8',
+        '----.':  '9',
+        '-----':  '0',
     }
     
     let result = ''
-    for (let num = 0; num < text.length; num++) {
-        char = text.toUpperCase()[num]
-
-        codeNumber = alfabeto[char].replace(/[-]/g, '11').replace(/[.]/g, '10')
-        if (codeNumber.length < 10) {
-            codeNumber = '0'.repeat(10 - codeNumber.length) + codeNumber
-        }
-
-        result = result + codeNumber
+    
+    morseCode = code.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ').split(' ')
+    for (let num = 0; num < morseCode.length; num++) {
+        result = result + alfabeto[morseCode[num]]
     }
     return result
 }
