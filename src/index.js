3e129d54b5de63f9dc25af00e30feab48f7a0af0
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2022-11-26 20:49:55 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-function toNumber(code) {
-    codeNumber = code.replace(/[-]/g, '11').replace(/[.]/g, '10')
-    if (codeNumber.length < 10) {
-        codeNumber = '0'.repeat(10 - codeNumber.length) + codeNumber
-    }
-    return codeNumber
-}
-
 function morseCodeDecoder(text) {
     let alfabeto = {
         A: '.-',
@@ -37,13 +29,15 @@ function morseCodeDecoder(text) {
     }
     
     let result = ''
-    console.log('1')
     for (let num = 0; num < text.length; num++) {
-        console.log('2')
         char = text.toUpperCase()[num]
-        console.log(alfabeto[char])
-        result = result + toNumber(alfabeto[char])
-        console.log(result)
+
+        codeNumber = alfabeto[char].replace(/[-]/g, '11').replace(/[.]/g, '10')
+        if (codeNumber.length < 10) {
+            codeNumber = '0'.repeat(10 - codeNumber.length) + codeNumber
+        }
+
+        result = result + codeNumber
     }
     return result
 }
