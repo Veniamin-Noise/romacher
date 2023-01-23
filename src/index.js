44dcae32f5112819d6c7744003b8131338d2e8da
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2023-01-23 10:43:04 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
-module.exports = function morseCodeDecoder(code) {
-    let alfabeto = {
-        '.-':     'a',
-        '-...':   'b',
-        '-.-.':   'c',
-        '-..':    'd',
-        '.':      'e',
-        '..-.':   'f',
-        '--.':    'g',
-        '....':   'h',
-        '..':     'i',
-        '.---':   'j',
-        '-.-':    'k',
-        '.-..':   'l',
-        '--':     'm',
-        '-.':     'n',
-        '---':    'o',
-        '.--.':   'p',
-        '--.-':   'q',
-        '.-.':    'r',
-        '...':    's',
-        '-':      't',
-        '..-':    'u',
-        '...-':   'v',
-        '.--':    'w',
-        '-..-':   'x',
-        '-.--':   'y',
-        '--..':   'z',
-        '.----':  '1',
-        '..---':  '2',
-        '...--':  '3',
-        '....-':  '4',
-        '.....':  '5',
-        '-....':  '6',
-        '--...':  '7',
-        '---..':  '8',
-        '----.':  '9',
-        '-----':  '0',
-    }
+module.exports = function decode(expr) {
     
     let result = ''
     
-    morseCode = code.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ').split(' ')
+    morseCode = expr.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ').split(' ')
     for (let num = 0; num < morseCode.length; num++) {
-        result = result + alfabeto[morseCode[num]]
+        result = result + MORSE_TABLE[morseCode[num]]
     }
     return result
 }
